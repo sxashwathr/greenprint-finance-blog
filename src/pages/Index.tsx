dd336df47b31c6d52e9a55f7c5ddd1a096bd6dcb
Sashwath <134656749+sxashwathr@users.noreply.github.com>
2025-06-07 14:23:41 -0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -10,28 +10,6 @@ const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <ThemeToggle />
-      
-      {/* Greenprint Brand */}
-      <div className="fixed top-4 left-4 z-50">
-        <div className="flex items-center space-x-2">
-          <div className="w-12 h-12 flex items-center justify-center">
-            {/* Light mode logo */}
-            <img 
-              src="/lovable-uploads/840f2312-113b-4db2-b16f-ba37c803ba71.png" 
-              alt="Greenprint Logo" 
-              className="w-10 h-10 dark:hidden"
-            />
-            {/* Dark mode logo */}
-            <img 
-              src="/lovable-uploads/840f2312-113b-4db2-b16f-ba37c803ba71.png" 
-              alt="Greenprint Logo" 
-              className="w-10 h-10 hidden dark:block"
-            />
-          </div>
-          <span className="font-bold text-lg text-foreground">Greenprint</span>
-        </div>
-      </div>
-      
       <Header />
       
       {/* Hero Section */}
