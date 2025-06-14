4d9a9e13a09ea864138c8f110f1b5ee49913cfc6
Sashwath <134656749+sxashwathr@users.noreply.github.com>
2025-06-14 16:14:49 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -19,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/greenprint-finance-blog">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/budgeting" element={<Budgeting />} />
